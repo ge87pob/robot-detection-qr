c421120ab4524a9296106a763c5f531d973404d1
ge87pob <ge87pob@mytum.de>
2025-12-03 16:46:36 +0100

    Initial Commit

---
+++ robot-qr-detection/robot_qr_detectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  robot_qr_detectionApp.swift
+//  robot-qr-detection
+//
+//  Created by Jan Chen on 03.12.25.
+//
+
+import SwiftUI
+
+@main
+struct robot_qr_detectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
